13112fd9d4d4c41b4b8bf64389da457dce42771b
Maxim Avanov <maxim.avanov@gmail.com>
2013-12-07 21:21:00 +0400

    Add a CLI entry point

--- pacific/cli/__init__.py
+++ pacific/cli/__init__.py
@@ -1,12 +1,44 @@
-""" Command-line tools' entry points.
 """
-import argparse
+Manage Pacific Apps from a command line.
+
+Usage:
+    pacific <command> <config>
+    pacific -h | --help
+
+Options:
+    -h, --help       Show this screen.
+    -V, --version    Show version.
+
+The most commonly used commands are:
+    run    Run a project instance.
+
+"""
+from pkg_resources import get_distribution
+
+from docopt import docopt
+from pyramid.scripts import pserve
 
 
 def main():
     """ Entry point for the ``pacific`` command.
     """
-    parser = argparse.ArgumentParser(
-        description='Manage Pacific Apps from a command line.'
-        )
-    args = parser.parse_args()
+    args = docopt(__doc__, argv=None, help=True,
+                  version=get_distribution('Pacific'),
+                  options_first=False)
+
+    return COMMANDS[args['<command>']](args)
+
+
+def cmd_run(args):
+    """
+
+    :param dict args:
+    :return:
+    """
+    pserve_argv = ['pserve', args['<config>']]
+    return pserve.main(pserve_argv)
+
+
+COMMANDS = {
+    'run': cmd_run
+}
