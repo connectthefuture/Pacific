64f39e450d98c8f05b91ff43ccdaa2dccc4b4d48
Maxim Avanov <maxim.avanov@gmail.com>
2014-01-25 14:47:49 +0400

    Fix #1

--- pacific/__init__.py
+++ pacific/__init__.py
@@ -8,22 +8,31 @@ def main(global_config, **settings):
 
     config.include('pacific.db')
 
-    setup_apps(config)
+    apps = get_apps_mapping(config)
+    for app_name, url_prefix in apps.items():
+        config.include(app_name, url_prefix)
 
     #config.add_static_view('static', 'static', cache_max_age=3600)
     #config.add_route('home', '/')
+
     config.scan()
+    # We must also scan applications' packages
+    for app_name in apps:
+        config.scan(app_name)
     return config.make_wsgi_app()
 
 
-def setup_apps(config):
+def get_apps_mapping(config):
     """
 
     :param config:
     :type config: :class:`pyramid.config.Configurator`
+    :return: mapping of app_name => app_url_prefix
+    :rtype: dict
     """
     apps_list = config.registry.settings['apps'].split(' ')
     apps = {}
     for app_mapping in apps_list:
         app_name, url_prefix = app_mapping.split('=>', 1)
         apps[app_name] = url_prefix
+    return apps
